b0d50981294da883385cbdbc5047ea5f9f6af26e
SSKirillSS <SSTroveSS@Gmail.com>
2024-07-31 05:13:52 +0300

    Initial commit

---
+++ src/main/java/it/hurts/octostudios/rarcompat/RARCompat.java
@@ -0,0 +1,12 @@
+package it.hurts.octostudios.rarcompat;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(RARCompat.MODID)
+public class RARCompat {
+    public static final String MODID = "rarcompat";
+
+    public RARCompat() {
+
+    }
+}
