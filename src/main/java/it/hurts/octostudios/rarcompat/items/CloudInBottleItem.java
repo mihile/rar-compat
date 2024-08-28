390a6aa68f15da099e2acfbcc6cba3d461e17a60
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-08-28 15:55:23 +0300

    update double jump

--- src/main/java/it/hurts/octostudios/rarcompat/items/CloudInBottleItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/CloudInBottleItem.java
@@ -3,6 +3,7 @@ package it.hurts.octostudios.rarcompat.items;
 import artifacts.registry.ModAttributes;
 import artifacts.registry.ModSoundEvents;
 import it.hurts.octostudios.rarcompat.items.base.WearableRelicItem;
+import it.hurts.sskirillss.relics.init.DataComponentRegistry;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilitiesData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
@@ -12,6 +13,7 @@ import it.hurts.sskirillss.relics.items.relics.base.data.leveling.misc.UpgradeOp
 import it.hurts.sskirillss.relics.utils.MathUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.ComponentUtils;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -29,14 +31,15 @@ import top.theillusivec4.curios.api.SlotContext;
 import java.util.Objects;
 
 public class CloudInBottleItem extends WearableRelicItem {
+    private boolean hasJumped = false;
 
     @Override
     public RelicData constructDefaultRelicData() {
         return RelicData.builder()
                 .abilities(AbilitiesData.builder()
-                        .ability(AbilityData.builder("fly")
-                                .stat(StatData.builder("time")
-                                        .initialValue(5D, 10D)
+                        .ability(AbilityData.builder("jump")
+                                .stat(StatData.builder("count")
+                                        .initialValue(2, 10D)
                                         .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.2D)
                                         .formatValue(value -> MathUtils.round(value, 1))
                                         .build())
@@ -53,27 +56,39 @@ public class CloudInBottleItem extends WearableRelicItem {
 
         LocalPlayer localPlayer = Minecraft.getInstance().player;
 
-        if (localPlayer != null && !player.onGround() && localPlayer.input.jumping) {
-            player.fallDistance = 0.0F;
-            double upwardsMotion = 0.7;
-
-            if (player.hasEffect(MobEffects.JUMP))
-                upwardsMotion += 0.1 * (double) (Objects.requireNonNull(player.getEffect(MobEffects.JUMP)).getAmplifier() + 1);
-
-            float direction = (float) ((double) player.getYRot() * Math.PI / 180.0);
-            double horizontalFactor = 3.5;
-
-            player.setDeltaMovement(player.getDeltaMovement().add(
-                    -Mth.sin(direction) / horizontalFactor,
-                    upwardsMotion - player.getDeltaMovement().y,
-                    Mth.cos(direction) / horizontalFactor
-            ));
-
-            player.hasImpulse = true;
-            player.awardStat(Stats.JUMP);
-
-            player.level().playSound(null, player, SoundEvents.WOOL_FALL, SoundSource.PLAYERS, 1.0F, 0.9F + player.getRandom().nextFloat() * 0.2F);
+        if (player.onGround()) {
+            stack.set(DataComponentRegistry.COUNT, 0);
+            hasJumped = false;
         }
 
+        if (localPlayer != null && !player.onGround()) {
+            if (localPlayer.input.jumping && !hasJumped && stack.getOrDefault(DataComponentRegistry.COUNT, 0) <= getStatValue(stack, "jump", "count")) {
+                hasJumped = true;
+                stack.set(DataComponentRegistry.COUNT, stack.getOrDefault(DataComponentRegistry.COUNT, 0) + 1);
+
+                if (stack.getOrDefault(DataComponentRegistry.COUNT, 0) > 1) {
+                    player.fallDistance = 0.0F;
+                    double upwardsMotion = 0.7;
+
+                    if (player.hasEffect(MobEffects.JUMP)) {
+                        upwardsMotion += 0.1 * (double) (Objects.requireNonNull(player.getEffect(MobEffects.JUMP)).getAmplifier() + 1);
+                    }
+
+                    float direction = (float) ((double) player.getYRot() * Math.PI / 180.0);
+                    double horizontalFactor = 3.5;
+
+                    player.setDeltaMovement(player.getDeltaMovement().add(
+                            -Mth.sin(direction) / horizontalFactor,
+                            upwardsMotion - player.getDeltaMovement().y,
+                            Mth.cos(direction) / horizontalFactor
+                    ));
+
+                    player.hasImpulse = true;
+                    player.awardStat(Stats.JUMP);
+                }
+            } else if (!localPlayer.input.jumping) {
+                hasJumped = false;
+            }
+        }
     }
 }
