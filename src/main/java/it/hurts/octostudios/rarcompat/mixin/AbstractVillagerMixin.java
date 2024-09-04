fb409580c02c3a1cc29d145c72d2a05ffbe215ad
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-09-04 14:19:39 +0300

    lil fix villager hat

--- src/main/java/it/hurts/octostudios/rarcompat/mixin/AbstractVillagerMixin.java
+++ src/main/java/it/hurts/octostudios/rarcompat/mixin/AbstractVillagerMixin.java
@@ -1,31 +1,32 @@
 package it.hurts.octostudios.rarcompat.mixin;
 
 import artifacts.registry.ModItems;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import it.hurts.octostudios.rarcompat.items.hat.VillagerHatItem;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.AbstractVillager;
-import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.trading.MerchantOffer;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.trading.MerchantOffers;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Villager.class)
-public abstract class VillagerMixin extends AbstractVillager {
+@Mixin(AbstractVillager.class)
+public abstract class AbstractVillagerMixin {
 
-    public VillagerMixin(EntityType<? extends AbstractVillager> p_35267_, Level p_35268_) {
-        super(p_35267_, p_35268_);
-    }
+    @Shadow
+    private Player tradingPlayer;
 
-    @Inject(method = "updateSpecialPrices", at = @At(value = "HEAD"))
-    private void increaseReputation(Player player, CallbackInfo ci) {
-        ItemStack relicStack = EntityUtils.findEquippedCurio(player, ModItems.VILLAGER_HAT.value());
+    @Shadow
+    protected MerchantOffers offers;
+
+    @Inject(method = "getOffers", at = @At(value = "HEAD"))
+    private void increaseReputation(CallbackInfoReturnable<MerchantOffers> cir) {
+        ItemStack relicStack = EntityUtils.findEquippedCurio(tradingPlayer, ModItems.VILLAGER_HAT.value());
         if (relicStack == null || !(relicStack.getItem() instanceof VillagerHatItem hat) || offers == null) return;
 
         for (MerchantOffer offer : this.offers) {
