7bc1d061ea8e0436b884fc60c582b3f1e36b2383
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-09-04 14:00:01 +0300

    fix villager hat

--- src/main/java/it/hurts/octostudios/rarcompat/mixin/VillagerMixin.java
+++ src/main/java/it/hurts/octostudios/rarcompat/mixin/VillagerMixin.java
@@ -4,22 +4,35 @@ import artifacts.registry.ModItems;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import it.hurts.octostudios.rarcompat.items.hat.VillagerHatItem;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Villager.class)
-public abstract class VillagerMixin {
+public abstract class VillagerMixin extends AbstractVillager {
 
-    @ModifyExpressionValue(method = "updateSpecialPrices", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/Villager;getPlayerReputation(Lnet/minecraft/world/entity/player/Player;)I"))
-    private int increaseReputation(int original, Player player) {
+    public VillagerMixin(EntityType<? extends AbstractVillager> p_35267_, Level p_35268_) {
+        super(p_35267_, p_35268_);
+    }
+
+    @Inject(method = "updateSpecialPrices", at = @At(value = "HEAD"))
+    private void increaseReputation(Player player, CallbackInfo ci) {
         ItemStack relicStack = EntityUtils.findEquippedCurio(player, ModItems.VILLAGER_HAT.value());
-        if (relicStack != null && relicStack.getItem() instanceof VillagerHatItem hat) {
-            double discount = hat.getStatValue(relicStack, "discount", "multiplier") * 4.5;
-            return (int) discount;
+        if (relicStack == null || !(relicStack.getItem() instanceof VillagerHatItem hat) || offers == null) return;
+
+        for (MerchantOffer offer : this.offers) {
+            int discounted = (int) Math.floor(offer.getItemCostA().count() * hat.getStatValue(relicStack, "discount", "multiplier") / 100);
+
+            offer.addToSpecialPriceDiff(-discounted);
         }
-        return original;
+
     }
 }
