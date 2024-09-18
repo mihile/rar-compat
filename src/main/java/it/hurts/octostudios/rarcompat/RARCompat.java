b478e7fd6cd67167afebe6650ea1c35fa17d63d1
SSKirillSS <SSTroveSS@Gmail.com>
2024-09-18 12:31:51 +0300

    Added compatibility with Relics 0.9.1

--- src/main/java/it/hurts/octostudios/rarcompat/RARCompat.java
+++ src/main/java/it/hurts/octostudios/rarcompat/RARCompat.java
@@ -1,27 +1,13 @@
 package it.hurts.octostudios.rarcompat;
 
-import artifacts.registry.ModItems;
-import it.hurts.octostudios.rarcompat.items.WearableRelicItem;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(RARCompat.MODID)
 public class RARCompat {
     public static final String MODID = "rarcompat";
 
     public RARCompat(IEventBus bus) {
-        bus.addListener(this::fillCreativeTabs);
-    }
 
-    public void fillCreativeTabs(final BuildCreativeModeTabContentsEvent event) {
-        if (event.getTab() == ModItems.CREATIVE_TAB.get()) {
-            for (Item item : BuiltInRegistries.ITEM.stream().toList()) {
-                if (item instanceof WearableRelicItem)
-                    event.accept(item);
-            }
-        }
     }
 }
