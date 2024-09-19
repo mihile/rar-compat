84aa890d70149e982ec8a9fe224f15b803126a5f
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-09-19 14:40:49 +0300

    New items, icons. Create lang

--- src/main/java/it/hurts/octostudios/rarcompat/items/necklace/ShockPendantItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/necklace/ShockPendantItem.java
@@ -1,4 +1,4 @@
-package it.hurts.octostudios.rarcompat.items.bunch;
+package it.hurts.octostudios.rarcompat.items.necklace;
 
 import artifacts.registry.ModItems;
 import it.hurts.octostudios.rarcompat.items.WearableRelicItem;
@@ -8,11 +8,15 @@ import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.StatData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.misc.UpgradeOperation;
+import it.hurts.sskirillss.relics.items.relics.base.data.misc.StatIcons;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
 import it.hurts.sskirillss.relics.utils.ParticleUtils;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -23,21 +27,29 @@ import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import java.awt.*;
 import java.util.Random;
 
-public class FlamePendantItem extends WearableRelicItem {
+public class ShockPendantItem extends WearableRelicItem {
 
     @Override
     public RelicData constructDefaultRelicData() {
         return RelicData.builder()
                 .abilities(AbilitiesData.builder()
-                        .ability(AbilityData.builder("fire")
+                        .ability(AbilityData.builder("poison")
+                                .stat(StatData.builder("multiplier")
+                                        .icon(StatIcons.MULTIPLIER)
+                                        .initialValue(0.05D, 0.1D)
+                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.075D)
+                                        .formatValue(value -> (int) MathUtils.round(value * 100, 1))
+                                        .build())
                                 .stat(StatData.builder("time")
-                                        .initialValue(1D, 10D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 2D)
+                                        .icon(StatIcons.DURATION)
+                                        .initialValue(3D, 7D)
+                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.5D)
                                         .formatValue(value -> MathUtils.round(value, 1))
                                         .build())
                                 .stat(StatData.builder("chance")
-                                        .initialValue(1D, 10D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 2D)
+                                        .icon(StatIcons.CHANCE)
+                                        .initialValue(10D, 90D)
+                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 9D)
                                         .formatValue(value -> MathUtils.round(value, 1))
                                         .build())
                                 .build())
@@ -46,26 +58,27 @@ public class FlamePendantItem extends WearableRelicItem {
                 .build();
     }
 
-    //@EventBusSubscriber
+    @EventBusSubscriber
     public static class Event {
 
         @SubscribeEvent
         public static void onReceivingDamage(LivingIncomingDamageEvent event) {
-            Entity attacker = event.getSource().getEntity();
+            Entity entityAttacker = event.getSource().getEntity();
 
-            if (!(event.getEntity() instanceof Player player) || attacker == null)
+            if (!(event.getEntity() instanceof Player player) || !(entityAttacker instanceof LivingEntity attacker))
                 return;
 
             Level level = attacker.level();
-            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.FLAME_PENDANT.value());
+            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.THORN_PENDANT.value());
 
-            if (!(stack.getItem() instanceof FlamePendantItem relic) || level.isClientSide) return;
+            if (!(stack.getItem() instanceof ShockPendantItem relic) || level.isClientSide) return;
             Random random = new Random();
 
-            if (random.nextInt(100) < relic.getStatValue(stack, "fire", "chance")) {
-                attacker.setRemainingFireTicks((int) relic.getStatValue(stack, "fire", "time"));
+            if (random.nextInt(100) < relic.getStatValue(stack, "poison", "chance")) {
+                attacker.hurt(event.getSource(), event.getAmount() * (float) relic.getStatValue(stack, "poison", "multiplier"));
+                attacker.addEffect(new MobEffectInstance(MobEffects.POISON, (int) relic.getStatValue(stack, "poison", "time") * 20, 1));
 
-                ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(200, 150 + random.nextInt(50), random.nextInt(50)), 0.4F, 20, 0.95F),
+                ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(50 + random.nextInt(50), 200 + random.nextInt(55), 50 + random.nextInt(50)), 0.4F, 20, 0.95F),
                         attacker.getX(), attacker.getY() + attacker.getBbHeight() / 2F, attacker.getZ(), 10, attacker.getBbWidth() / 2F, attacker.getBbHeight() / 2F, attacker.getBbWidth() / 2F, 0.025F);
             }
         }
--- src/main/java/it/hurts/octostudios/rarcompat/items/necklace/ThornPendantItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/necklace/ThornPendantItem.java
@@ -1,4 +1,4 @@
-package it.hurts.octostudios.rarcompat.items.bunch;
+package it.hurts.octostudios.rarcompat.items.necklace;
 
 import artifacts.registry.ModItems;
 import it.hurts.octostudios.rarcompat.items.WearableRelicItem;
@@ -8,14 +8,14 @@ import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.StatData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.misc.UpgradeOperation;
+import it.hurts.sskirillss.relics.items.relics.base.data.misc.StatIcons;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
 import it.hurts.sskirillss.relics.utils.ParticleUtils;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -32,18 +32,15 @@ public class ThornPendantItem extends WearableRelicItem {
     public RelicData constructDefaultRelicData() {
         return RelicData.builder()
                 .abilities(AbilitiesData.builder()
-                        .ability(AbilityData.builder("poison")
-                                .stat(StatData.builder("multiplier")
-                                        .initialValue(0.05D, 0.1D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.075D)
-                                        .formatValue(value -> (int) MathUtils.round(value * 100, 1))
-                                        .build())
-                                .stat(StatData.builder("time")
-                                        .initialValue(3D, 7D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.5D)
+                        .ability(AbilityData.builder("lightning")
+                                .stat(StatData.builder("damage")
+                                        .icon(StatIcons.DEALT_DAMAGE)
+                                        .initialValue(1D, 10D)
+                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 1D)
                                         .formatValue(value -> MathUtils.round(value, 1))
                                         .build())
                                 .stat(StatData.builder("chance")
+                                        .icon(StatIcons.CHANCE)
                                         .initialValue(10D, 90D)
                                         .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 9D)
                                         .formatValue(value -> MathUtils.round(value, 1))
@@ -54,27 +51,30 @@ public class ThornPendantItem extends WearableRelicItem {
                 .build();
     }
 
-    //  @EventBusSubscriber
+    @EventBusSubscriber
     public static class Event {
 
         @SubscribeEvent
         public static void onReceivingDamage(LivingIncomingDamageEvent event) {
-            Entity entityAttacker = event.getSource().getEntity();
+            Entity attacker = event.getSource().getEntity();
 
-            if (!(event.getEntity() instanceof Player player) || !(entityAttacker instanceof LivingEntity attacker))
+            if (!(event.getEntity() instanceof Player player) || attacker == null)
                 return;
 
             Level level = attacker.level();
-            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.THORN_PENDANT.value());
+            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.SHOCK_PENDANT.value());
 
             if (!(stack.getItem() instanceof ThornPendantItem relic) || level.isClientSide) return;
             Random random = new Random();
 
-            if (random.nextInt(100) < relic.getStatValue(stack, "poison", "chance")) {
-                attacker.hurt(event.getSource(), event.getAmount() * (float) relic.getStatValue(stack, "poison", "multiplier"));
-                attacker.addEffect(new MobEffectInstance(MobEffects.POISON, (int) relic.getStatValue(stack, "poison", "time") * 20, 1));
+            if (random.nextInt(100) < relic.getStatValue(stack, "lightning", "chance")) {
+                LightningBolt lightningBolt = new LightningBolt(EntityType.LIGHTNING_BOLT, level);
 
-                ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(50 + random.nextInt(50), 200 + random.nextInt(55), 50 + random.nextInt(50)), 0.4F, 20, 0.95F),
+                lightningBolt.setPos(attacker.position());
+                lightningBolt.setDamage((float) relic.getStatValue(stack, "lightning", "damage"));
+                level.addFreshEntity(lightningBolt);
+
+                ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(random.nextInt(50), random.nextInt(50), 50 + random.nextInt(55)), 0.4F, 20, 0.95F),
                         attacker.getX(), attacker.getY() + attacker.getBbHeight() / 2F, attacker.getZ(), 10, attacker.getBbWidth() / 2F, attacker.getBbHeight() / 2F, attacker.getBbWidth() / 2F, 0.025F);
             }
         }
