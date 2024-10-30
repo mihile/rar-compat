861f20e7b9495d4608963e9618bfb6102acdd9ca
Amiri163 <167354679+Amiri000@users.noreply.github.com>
2024-10-30 21:39:59 +0300

    remove belt in model render artifacts

---
+++ src/main/java/it/hurts/octostudios/rarcompat/mixin/BeltModelMixin.java
@@ -0,0 +1,41 @@
+package it.hurts.octostudios.rarcompat.mixin;
+
+import artifacts.client.item.model.BeltModel;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.world.item.trading.MerchantOffer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.client.model.HumanoidModel.createMesh;
+
+@Mixin(BeltModel.class)
+public class BeltModelMixin {
+
+    @Inject(method = "createBelt ", at = @At(value = "HEAD"), cancellable = true)
+    private static void createBelt(CubeListBuilder charm, CallbackInfoReturnable<MeshDefinition> cir) {
+        CubeDeformation deformation = new CubeDeformation(0.5F);
+        MeshDefinition mesh = createMesh(CubeDeformation.NONE, 0);
+
+        mesh.getRoot().addOrReplaceChild(
+                "body",
+                CubeListBuilder.create()
+                        .texOffs(0, 0),
+                PartPose.ZERO
+        );
+
+        mesh.getRoot().getChild("body").addOrReplaceChild(
+                "charm",
+                charm,
+                PartPose.ZERO
+        );
+
+        cir.setReturnValue(mesh);
+    }
+}
