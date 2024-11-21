d6c01968e316ea18397299a961528120468e8b80
Amiri000 <iuak228@gmail.com>
2024-11-21 21:45:21 +0300

    new item Obsidian Skull

--- src/main/java/it/hurts/octostudios/rarcompat/items/belt/ObsidianSkullItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/belt/ObsidianSkullItem.java
@@ -1,6 +1,8 @@
 package it.hurts.octostudios.rarcompat.items.belt;
 
+import artifacts.registry.ModItems;
 import it.hurts.octostudios.rarcompat.items.WearableRelicItem;
+import it.hurts.sskirillss.relics.init.DataComponentRegistry;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilitiesData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
@@ -11,7 +13,20 @@ import it.hurts.sskirillss.relics.items.relics.base.data.loot.LootData;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.misc.LootCollections;
 import it.hurts.sskirillss.relics.items.relics.base.data.misc.StatIcons;
 import it.hurts.sskirillss.relics.items.relics.base.data.research.ResearchData;
+import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.ArmorHurtEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingShieldBlockEvent;
+import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
+import top.theillusivec4.curios.api.SlotContext;
 
 public class ObsidianSkullItem extends WearableRelicItem {
 
@@ -22,9 +37,9 @@ public class ObsidianSkullItem extends WearableRelicItem {
                         .ability(AbilityData.builder("buffer")
                                 .stat(StatData.builder("duration")
                                         .icon(StatIcons.DURATION)
-                                        .initialValue(2D, 3D)
-                                        .upgradeModifier(UpgradeOperation.ADD, 0.1D)
-                                        .formatValue(value -> MathUtils.round(value, 1))
+                                        .initialValue(1D, 3D)
+                                        .upgradeModifier(UpgradeOperation.ADD, 0.2D)
+                                        .formatValue(value -> MathUtils.round(value * 2, 1))
                                         .build())
                                 .research(ResearchData.builder()
                                         .star(0, 8, 25).star(1, 3, 16).star(2, 6, 8).star(3, 12, 4)
@@ -40,4 +55,62 @@ public class ObsidianSkullItem extends WearableRelicItem {
                 .build();
     }
 
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        if (!(slotContext.entity() instanceof Player player) || player.tickCount % 20 != 0)
+            return;
+
+        addTime(stack, 1);
+
+        int time = getTime(stack);
+        int charges = getCharges(stack);
+        double stat = getStatValue(stack, "buffer", "duration") * 2;
+
+        if (time >= 3 && charges > 0 && charges < stat) {
+            addCharges(stack, -1);
+            addTime(stack, -time);
+        } else if (time >= 60 && charges > stat) {
+            addCharges(stack, -charges);
+            addTime(stack, -time);
+        }
+    }
+
+    public static void addTime(ItemStack stack, int time) {
+        stack.set(DataComponentRegistry.TIME, getTime(stack) + time);
+    }
+
+    public static int getTime(ItemStack stack) {
+        return stack.getOrDefault(DataComponentRegistry.TIME, 0);
+    }
+
+    public static void addCharges(ItemStack stack, int count) {
+        stack.set(DataComponentRegistry.CHARGE, getCharges(stack) + count);
+    }
+
+    public static int getCharges(ItemStack stack) {
+        return stack.getOrDefault(DataComponentRegistry.CHARGE, 0);
+    }
+
+    @EventBusSubscriber
+    public static class ObsidianSkull {
+
+        @SubscribeEvent
+        public static void onPreGetDamage(LivingDamageEvent.Pre event) {
+            if (!(event.getEntity() instanceof Player player) || !event.getSource().is(DamageTypeTags.IS_FIRE))
+                return;
+
+            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.OBSIDIAN_SKULL.value());
+
+            if (!(stack.getItem() instanceof ObsidianSkullItem relic))
+                return;
+
+            double stat = relic.getStatValue(stack, "buffer", "duration") * 2;
+
+            addCharges(stack, 1);
+            addTime(stack, -getTime(stack));
+
+            if (getCharges(stack) <= stat)
+                event.setNewDamage(0);
+        }
+    }
 }
