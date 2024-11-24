6995a1ff6e4f084caeb91f05a4aec8a5ca96b451
Amiri000 <iuak228@gmail.com>
2024-11-24 18:48:40 +0300

    revision Pickaxe Heater Item

--- src/main/java/it/hurts/octostudios/rarcompat/items/hands/PickaxeHeaterItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/hands/PickaxeHeaterItem.java
@@ -4,6 +4,8 @@ import artifacts.registry.ModItems;
 import it.hurts.octostudios.rarcompat.items.WearableRelicItem;
 import it.hurts.sskirillss.relics.init.DataComponentRegistry;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicData;
+import it.hurts.sskirillss.relics.items.relics.base.data.cast.CastData;
+import it.hurts.sskirillss.relics.items.relics.base.data.cast.misc.CastType;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilitiesData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingData;
@@ -15,26 +17,31 @@ import it.hurts.sskirillss.relics.items.relics.base.data.misc.StatIcons;
 import it.hurts.sskirillss.relics.items.relics.base.data.research.ResearchData;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
+import it.hurts.sskirillss.relics.utils.ParticleUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.SimpleContainer;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 import net.neoforged.neoforge.event.level.BlockDropsEvent;
-import net.neoforged.neoforge.event.level.BlockEvent;
 import top.theillusivec4.curios.api.SlotContext;
 
+import java.awt.*;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Random;
 
 public class PickaxeHeaterItem extends WearableRelicItem {
 
@@ -43,23 +50,14 @@ public class PickaxeHeaterItem extends WearableRelicItem {
         return RelicData.builder()
                 .abilities(AbilitiesData.builder()
                         .ability(AbilityData.builder("heater")
-                                .stat(StatData.builder("heat")
-                                        .icon(StatIcons.SIZE)
-                                        .initialValue(0.2D, 0.4D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.15D)
-                                        .formatValue(value -> MathUtils.round(value * 100, 1))
+                                .active(CastData.builder()
+                                        .type(CastType.TOGGLEABLE)
                                         .build())
-                                .stat(StatData.builder("duration")
-                                        .icon(StatIcons.DURATION)
-                                        .initialValue(0.2D, 0.4D)
+                                .stat(StatData.builder("capacity")
+                                        .icon(StatIcons.CAPACITY)
+                                        .initialValue(5D, 8D)
                                         .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.15D)
-                                        .formatValue(value -> MathUtils.round(value * 100, 1))
-                                        .build())
-                                .stat(StatData.builder("modifier")
-                                        .icon(StatIcons.MODIFIER)
-                                        .initialValue(0.2D, 0.3D)
-                                        .upgradeModifier(UpgradeOperation.MULTIPLY_BASE, 0.1D)
-                                        .formatValue(value -> MathUtils.round(value * 100, 1))
+                                        .formatValue(value -> MathUtils.round(value, 0))
                                         .build())
                                 .research(ResearchData.builder()
                                         .star(0, 11, 28).star(1, 11, 18).star(2, 19, 24)
@@ -77,19 +75,19 @@ public class PickaxeHeaterItem extends WearableRelicItem {
 
     @Override
     public void curioTick(SlotContext slotContext, ItemStack stack) {
-        if (!(slotContext.entity() instanceof Player player) || player.tickCount % 20 != 0)
+        if (!(slotContext.entity() instanceof Player player) || player.tickCount % 100 != 0)
             return;
-        addTime(stack, 1);
 
-
+        if (MathUtils.round(getStatValue(stack, "heater", "capacity"), 0) > getCharges(stack))
+            addCharge(stack, 1);
     }
 
-    public static void addTime(ItemStack stack, int val) {
-        stack.set(DataComponentRegistry.TIME, getTime(stack) + val);
+    public static void addCharge(ItemStack stack, int val) {
+        stack.set(DataComponentRegistry.CHARGE, getCharges(stack) + val);
     }
 
-    public static int getTime(ItemStack stack) {
-        return stack.getOrDefault(DataComponentRegistry.TIME, 0);
+    public static int getCharges(ItemStack stack) {
+        return stack.getOrDefault(DataComponentRegistry.CHARGE, 0);
     }
 
     @EventBusSubscriber
@@ -100,26 +98,50 @@ public class PickaxeHeaterItem extends WearableRelicItem {
             if (!(event.getBreaker() instanceof Player player))
                 return;
 
-            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.PICKAXE_HEATER.value());
+            Level level = player.level();
 
-            if (!(stack.getItem() instanceof PickaxeHeaterItem relic))
+            ItemStack stack = EntityUtils.findEquippedCurio(player, ModItems.PICKAXE_HEATER.value());
+            List<ItemStack> smeltingResult = getSmeltingResult(new ItemStack(event.getState().getBlock()), (ServerLevel) level).stream().toList();
+
+            if (smeltingResult.isEmpty() || !(stack.getItem() instanceof PickaxeHeaterItem relic) || !relic.isAbilityTicking(stack, "heater") || getCharges(stack) < 1)
                 return;
 
-            Level level = player.level();
             BlockPos pos = event.getPos();
 
-            for (ItemStack itemStack : getSmeltingResult(new ItemStack(event.getState().getBlock()), (ServerLevel) level).stream().toList()) {
+            for (ItemStack itemStack : smeltingResult) {
                 ItemEntity itemEntity = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemStack);
                 level.addFreshEntity(itemEntity);
 
                 event.setCanceled(true);
             }
+
+            spawnBurstParticles(level, pos);
+            addCharge(stack, -1);
+        }
+
+        public static void spawnBurstParticles(Level level, BlockPos centerPos) {
+            double centerX = centerPos.getX() + 0.5;
+            double centerY = centerPos.getY() + 0.5;
+            double centerZ = centerPos.getZ() + 0.5;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 25; i++) {
+                ((ServerLevel) level).sendParticles(
+                        ParticleUtils.constructSimpleSpark(
+                                new Color(150 + random.nextInt(106), random.nextInt(50), 50 + random.nextInt(51), 255),
+                                0.6F, 20, 0.85F),
+                        centerX, centerY, centerZ,
+                        1,
+                        0.3, 0.3, 0.3,
+                        0.01
+                );
+            }
         }
 
         public static Optional<ItemStack> getSmeltingResult(ItemStack stack, ServerLevel level) {
-            if (stack.isEmpty()) {
+            if (stack.isEmpty())
                 return Optional.empty();
-            }
 
             Optional<RecipeHolder<SmeltingRecipe>> optionalRecipe =
                     Objects.requireNonNull(level.getServer()).getRecipeManager()
@@ -128,5 +150,4 @@ public class PickaxeHeaterItem extends WearableRelicItem {
             return optionalRecipe.map(recipeHolder -> recipeHolder.value().getResultItem(level.registryAccess()));
         }
     }
-
 }
