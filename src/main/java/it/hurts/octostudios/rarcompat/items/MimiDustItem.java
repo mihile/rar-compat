4a8471d2b54cfac46a869d7dd70694b5505d0e3c
SSKirillSS <SSTroveSS@Gmail.com>
2025-01-10 09:11:24 +0300

    Mimi-Dust review

--- src/main/java/it/hurts/octostudios/rarcompat/items/MimiDustItem.java
+++ src/main/java/it/hurts/octostudios/rarcompat/items/MimiDustItem.java
@@ -7,6 +7,7 @@ import it.hurts.sskirillss.relics.items.misc.CreativeContentConstructor;
 import it.hurts.sskirillss.relics.items.misc.ICreativeTabContent;
 import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
 import it.hurts.sskirillss.relics.utils.ParticleUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
@@ -30,31 +31,34 @@ public class MimiDustItem extends Item implements ICreativeTabContent {
         var level = context.getLevel();
 
         if (level.isClientSide())
-            return InteractionResult.FAIL;
+            return InteractionResult.PASS;
 
         var pos = context.getClickedPos();
 
         if (level.getBlockState(pos).getBlock() != Blocks.CHEST || !(level.getBlockEntity(pos) instanceof ChestBlockEntity chest))
             return InteractionResult.FAIL;
 
-        var mimic = new MimicEntity(ModEntityTypes.MIMIC.value(), level);
-        var persistentData = mimic.getPersistentData();
+        var count = 0;
 
         for (int i = 0; i < chest.getContainerSize(); i++)
             if (chest.getItem(i).getItem() instanceof IRelicItem) {
                 chest.setItem(i, ItemStack.EMPTY);
 
-                persistentData.putInt("relicCount", persistentData.getInt("relicCount") + 1);
+                count++;
             }
 
-        if (persistentData.getInt("relicCount") < 1)
+        if (count == 0)
             return InteractionResult.FAIL;
+
+        context.getItemInHand().shrink(1);
 
         level.removeBlock(pos, true);
 
-        mimic.setPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+        var mimic = new MimicEntity(ModEntityTypes.MIMIC.value(), level);
 
-        context.getItemInHand().shrink(1);
+        mimic.getPersistentData().putInt("relicCount", count);
+        mimic.setPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+        mimic.setTarget(context.getPlayer());
 
         level.addFreshEntity(mimic);
 
@@ -63,8 +67,8 @@ public class MimiDustItem extends Item implements ICreativeTabContent {
 
         level.playSound(null, mimic.blockPosition(), SoundEvents.ALLAY_AMBIENT_WITHOUT_ITEM, mimic.getSoundSource(), 1.3F, 0.75F + mimic.getRandom().nextFloat());
 
-        ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(255, 50 + random.nextInt(50), 200 + random.nextInt(55), 255)   ,
-                1F, 60, 0.85F), center.x(), center.y(), center.z(), 25, 0.3, 0.3, 0.3, 0.05);
+        ((ServerLevel) level).sendParticles(ParticleUtils.constructSimpleSpark(new Color(200 + random.nextInt(50), 50 + random.nextInt(50), 200 + random.nextInt(50)),
+                0.25F, 60, 0.95F), center.x(), center.y(), center.z(), 50, 0.3, 0.3, 0.3, 0.015);
 
         return InteractionResult.SUCCESS;
     }
@@ -76,6 +80,8 @@ public class MimiDustItem extends Item implements ICreativeTabContent {
 
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag flag) {
-        tooltip.add(Component.translatable("tooltip.rarcompat.mimi_dust"));
+        tooltip.add(Component.empty());
+
+        tooltip.add(Component.translatable("tooltip.rarcompat.mimi_dust").withStyle(ChatFormatting.DARK_PURPLE));
     }
 }
